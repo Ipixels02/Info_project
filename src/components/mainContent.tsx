16e8634ba63d24a58f0e897d08540c119494d91a
Басыров Артур <ar4iking19@gmail.com>
2021-08-06 20:23:13 +0500

    Добавлен фон particles на главную

--- src/components/mainContent.tsx
+++ src/components/mainContent.tsx
@@ -28,8 +28,10 @@ import {
 import {FaPlayCircle} from 'react-icons/fa';
 import Particles from "react-tsparticles";
 import {particlesConfig} from "./ParticlesConfig";
+import {useMediaQuery} from "react-responsive";
 
 export const MainContent = () => {
+    const isTabletOrMobile = useMediaQuery({query: '(max-width: 991px)'});
     return (
         <>
             <Particles
@@ -41,24 +43,46 @@ export const MainContent = () => {
                     <h5>Передовая инвестиционная компания</h5>
                     <h1>Сайт-Агрегатор инвестиционных <br/> и финансовых проектов</h1>
                     <div
-                        style={{display: "flex", justifyContent: "center", alignItems: "center", padding: "10px 15px"}}>
-                        <Button color={"cyan"} size={'lg'}>Участвовать в программе</Button>
-                        <div style={{width: "32px"}}><FaPlayCircle width={"16"} size={"1x"}/>Посмотреть видео</div>
+                        style={{display: "flex",fontSize:"20px", justifyContent: "center", alignItems: "center", padding: "10px 15px"}}>
+                        <Button color={"cyan"} size={'lg'}><span style={{fontSize:"20px"}}>Участвовать в программе</span></Button>
+                        <div onClick={()=>{alert("IDIDIDIDI")}} style={{marginLeft: "40px",padding:"12px 15px 10px 15px", border: "solid 2px white", borderRadius: "100px", display: "flex", alignItems:"center"}}>
+                            <div style={{width: "54px"}}><FaPlayCircle width={"16"} size={"1x"}/></div>
+                            <span style={{marginLeft: "10px"}}>Посмотреть видео</span>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className={"about-block"}>
                 <img className={"styleImg"} src={process.env.PUBLIC_URL + '/images/bg_about_us.svg'}/>
-                <FlexboxGrid className={"about_grid"} justify="space-between" align="middle">
-                    <FlexboxGrid.Item colspan={6}>
+                <FlexboxGrid style={{padding: "0 4%"}} justify="space-between" align="top">
+                    <FlexboxGrid.Item componentClass={Col} colspan={16} md={8} order={1}>
                         <div className={"about_company"}>
-                            <h1 style={{fontSize: "44px", fontWeight: "bold", color: "black"}}>О компании</h1>
+                            <h1 style={{verticalAlign: "top"}}>О компании</h1>
+                            <h5 style={{color: "#7A7A7A"}}>Помогаем обрести финансовую подушку с 2021 года</h5>
+                        </div>
+                    </FlexboxGrid.Item>
+                    <FlexboxGrid.Item componentClass={Col} colspan={12} md={6}
+                                      order={isTabletOrMobile ? 3 : 2}></FlexboxGrid.Item>
+                    <FlexboxGrid.Item style={{margin:"auto 0"}}  componentClass={Col} colspan={8} md={8} order={isTabletOrMobile ? 2 : 3}>
+                        <Button  color={"cyan"} size={'lg'}>Приссоединится</Button>
+                    </FlexboxGrid.Item>
+                </FlexboxGrid>
+
+                <FlexboxGrid justify="space-between" align="top">
+                    <FlexboxGrid.Item componentClass={Col} colspan={16} md={6}>
+                        <div className={"about_company"}>
+                            <h1 style={{verticalAlign: "top"}}>О
+                                компании</h1>
                             <h5>Помогаем обрести финансовую подушку с 2021 года</h5>
                         </div>
                     </FlexboxGrid.Item>
-                    <FlexboxGrid.Item colspan={6}>colspan={6}</FlexboxGrid.Item>
-                    <FlexboxGrid.Item colspan={8}>colspan={6}</FlexboxGrid.Item>
+                    <FlexboxGrid.Item componentClass={Col} colspan={12} md={6}
+                                      style={{backgroundColor: "black"}}>f</FlexboxGrid.Item>
+                    <FlexboxGrid.Item componentClass={Col} colspan={12} md={8}
+                                      style={{backgroundColor: "white"}}>f</FlexboxGrid.Item>
                 </FlexboxGrid>
+
+
             </div>
         </>
     );
